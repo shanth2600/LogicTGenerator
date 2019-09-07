0297f4a2855866c987a7a01c5d5fb7946f795ba9
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-09-07 12:08:25 -0700

    starting java

--- src/Java.hs
+++ src/Java.hs
@@ -1,1 +1,13 @@
 module Java where
+
+newtype ClassName = ClassName String    
+newtype MethodName = MethodName String
+
+data Type = IntType | BooleanType | DoubleType | ClassType ClassName
+
+data Call = Call Expression MethodName [Expression]
+
+data Expression
+
+class PrettyString a where
+    prettyString :: a -> String
