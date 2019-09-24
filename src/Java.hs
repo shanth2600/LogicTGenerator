8ca0ca5157c78ec4e13d64f36afe70d3fdd03b59
Shant Hairapetian <shant.hairapetian@metalab.csun.edu>
2019-09-24 09:49:24 -0700

    finished java syntax

--- src/Java.hs
+++ src/Java.hs
@@ -1,13 +1,38 @@
 module Java where
 
-newtype ClassName = ClassName String    
-newtype MethodName = MethodName String
+import Data.List
 
-data Type = IntType | BooleanType | DoubleType | ClassType ClassName
+data Variable = Variable String deriving Show
+data MethodName = MethodName String deriving Show
+data ClassName = ClassName String deriving Show 
 
-data Call = Call Expression MethodName [Expression]
+data Type = IntType | BooleanType | DoubleType | ClassType ClassName deriving Show
 
-data Expression
+data Call = Call Expression MethodName [Expression] deriving Show
 
-class PrettyString a where
-    prettyString :: a -> String
+data Expression = IntLiteral Int
+                | BooleanLiteral Bool
+                | DoubleLiteral Double
+                | StringLiteral String
+                | NewExpression ClassName [Expression] 
+                | CallExpression Call
+                deriving Show
+
+data Statement = VariableDeclaration Type Variable Expression
+               | CallStatement Call
+               deriving Show
+
+data JavaTest = JavaTest [Statement]
+
+instance Show JavaTest where
+    show (JavaTest statements) =
+        "public class test {\n" ++
+        "    public static void main(String[] args) {\n" ++
+        "   " ++
+        intercalate "\n " (map show statements) ++
+        "   }\n" ++
+        "{\n"
+
+data ClassSpec = ClassSpec ClassName [ClassName] [ConstructorSpec] [MethodSpec]
+data ConstructorSpec = ConstructorSpec [Type]
+data MethodSpec = MethodSpec MethodName [Type] (Maybe Type)
